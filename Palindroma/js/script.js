47a806e4c44b89f656a598b33d10c72911330da1
Titho93 <titho10.93@gmail.com>
2023-09-15 14:16:43 +0200

    [add]two folds

---
+++ Palindroma/js/script.js
@@ -0,0 +1,9 @@
+// Palindroma 
+
+// 2. Creare la funzione 
+// 3. all'interno della funzione mettere una condizione if 
+// 4. nell'if specificare che se la parola invertita è uguale allora è palindroma
+// altrimenti non lo è.
+
+
+// 1. Crea un prompt dove chiedi all'utente di inserire una parola
--- Pariedispari/js/script.js
+++ Pariedispari/js/script.js
@@ -1,10 +1,3 @@
-// Palindroma 
-// 1. Crea un prompt dove chiedi all'utente di inserire una parola
-// 2. Creare la funzione 
-// 3. all'interno della funzione mettere una condizione if 
-// 4. nell'if specificare che se la parola invertita è uguale allora è palindroma
-// altrimenti non lo è.
-
 // Pari e Dispari
 // 1. Creo un prompt dove chiedo all'utente di inserire o pari o dispari
 // 2. Creo un'altro prompt dove chiedo all'utente di scegliere un numero compreso tra 1 e 5
