59c16d913c96bca426c0426933f79c2959925fe5
Titho93 <titho10.93@gmail.com>
2023-09-15 15:31:28 +0200

    [add]innerHtml for message

--- Pariedispari/js/script.js
+++ Pariedispari/js/script.js
@@ -5,11 +5,11 @@ const pariDispari = prompt('Scegli "pari" o "dispari"')
 
 // 2. Creo un'altro prompt dove chiedo all'utente di scegliere un numero compreso tra 1 e 5
 const numberUser = parseInt(prompt('Scrivi un numero da 1 a 5'));
-
+console.log(numberUser)
 // 4. Creo una costante con la funzione per il numero random 
 const numberGuest = randomNumber(1, 5); 
 let numberValid = true;
-let messaggio;
+let message;
 
 // 3. creo all'interno di una funzione un numero random che sarà il valore dettato dal computer
 function randomNumber(min, max){
@@ -17,7 +17,7 @@ function randomNumber(min, max){
   
   return random;
 }
-
+ console.log(numberGuest)
 // 5. tramite un if effettuo la somma dei due numeri ricevuti e dichiaro chi ha vinto nei vari casi
 if(numberValid){
   const somma = numberUser + numberGuest;
@@ -25,11 +25,12 @@ if(numberValid){
 
 
   if((!(somma % 2) && pariDispari === 'pari') || (somma % 2 && pariDispari === 'dispari')){
-    messaggio = 'Vince l\'utente';
+    message = 'User Win';
   }else{
-    messaggio = 'Vince il PC';
+    message = 'Guest win';
   }
 
+  message += `  The Selection is ${pariDispari} the User select ${numberUser} and Guest select ${numberGuest}` ;
+  document.getElementById('output').innerHTML = message;
 }
 
-console.log(messaggio);
