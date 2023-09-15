16a2f12bd1bc6cb19bc27a003a7cc7132d79b021
Titho93 <titho10.93@gmail.com>
2023-09-15 16:18:36 +0200

    [add] if and message

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,16 +1,26 @@
 // Palindroma 
 
-
-// 3. Mettere una condizione if 
-// 4. Nell'if specificare che se la parola invertita è uguale allora è palindroma
-// altrimenti non lo è.
-
-
 // 1. Crea un prompt dove chiedi all'utente di inserire una parola
 const word = prompt('inserisci una parola');
+
+let messaggio;
 
 // 2. Creare la funzione 
 function reverseWord(word){
   return word === word.split('').reverse().join('');
 } 
 console.log(word)
+
+// 3. Mettere una condizione if 
+if (reverseWord(word)) {
+  // 4. Nell'if specificare che se la parola invertita è uguale allora è palindroma
+  message = word + " è una parola palindroma.";
+
+  // altrimenti non lo è.
+} else {
+  message = word + " non è una parola palindroma.";
+
+}
+
+// Creare un messaggio
+console.log(message)
