100b934a6c2da4739242f73b94c5cd8dc59f3511
Titho93 <titho10.93@gmail.com>
2023-09-15 14:11:14 +0200

    [add]file html and file js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Palindroma 
+// 1. Crea un prompt dove chiedi all'utente di inserire una parola
+// 2. Creare la funzione 
+// 3. all'interno della funzione mettere una condizione if 
+// 4. nell'if specificare che se la parola invertita è uguale allora è palindroma
+// altrimenti non lo è.
+
+// Pari e Dispari
+// 1. Creo un prompt dove chiedo all'utente di inserire o pari o dispari
+// 2. Creo un'altro prompt dove chiedo all'utente di scegliere un numero compreso tra 1 e 5
+// 3. creo all'interno di una funzione un numero random che sarà il valore dettato dal computer
+// 4. tramite un if effettuo la somma dei due numeri ricevuti e dichiaro chi ha vinto nei vari casi
